3401dd0e04cb1ee145df8339387c64468c5e2002
ibelieve3212 <70727665+ibelieve3212@users.noreply.github.com>
2024-06-24 19:01:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,9 +19,10 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo -i '$a src-git Openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
-echo -i '$a src-git DDNSGO https://github.com/jeessy2/ddns-go.git' feeds.conf.default
+#echo -i '$a src-git DDNSGO https://github.com/jeessy2/ddns-go.git' feeds.conf.default
 #echo -i '$a src-git smartdns https://github.com/pymumu/luci-app-smartdns/tree/lede.git' feeds.conf.default
 #echo -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' feeds.conf.default
 #echo -i '$a src-git zerotier https://github.com/rufengsuixing/luci-app-zerotier.git' feeds.conf.default
 #echo -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
 #sed -i '$a src-git immluci https://github.com/immortalwrt/luci.git' feeds.conf.default
+git clone --depth=1 https://github.com/jeessy2/ddns-go.git package/luci-app-ddnsgo
