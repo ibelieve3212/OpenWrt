6b59b4901c6dd3d1d60d7c15e7d1d9db97d9a7e7
ibelieve3212 <70727665+ibelieve3212@users.noreply.github.com>
2024-06-24 12:40:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,10 +18,10 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#echo -i '$a src-git Openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
-echo -i '$a src-git DDNS-GO https://github.com/sirpdboy/luci-app-ddns-go.git' feeds.conf.default
+echo -i '$a src-git Openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+echo -i '$a src-git DDNS-GO https://github.com/jeessy2/ddns-go.git' feeds.conf.default
 #echo -i '$a src-git smartdns https://github.com/pymumu/luci-app-smartdns/tree/lede.git' feeds.conf.default
 #echo -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' feeds.conf.default
 #echo -i '$a src-git zerotier https://github.com/rufengsuixing/luci-app-zerotier.git' feeds.conf.default
 #echo -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
-sed -i '$a src-git immluci https://github.com/immortalwrt/luci.git' feeds.conf.default
+#sed -i '$a src-git immluci https://github.com/immortalwrt/luci.git' feeds.conf.default
